2b1cfe93ffac56d06eaa76a644ef75beecd97484
Christopher Johnson <chris@clickonchris.com>
2013-05-26 18:21:43 -0500

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IinstinctsCom::Application.config.secret_token = '92aceba85e0cf9a8b56663696678aec7936e77ecfbc023cf6470209753bf99f9c6ee636c2e897b25943bc53d82478f7c9a6442060535d51c17804a5a68eae18a'
