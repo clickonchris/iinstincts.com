af7424a1a034851c41f5261978b4e0f510ede650
Christopher Johnson <chris@clickonchris.com>
2013-05-28 08:34:11 -0500

    pagescroller

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ IinstinctsCom::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  
+  resources :home
 
   # Sample resource route with options:
   #   resources :products do
